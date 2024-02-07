459a34564f71f4937a642bdba8ff89cf8c4f184e
杉本博陽 <hiroaki.sugimoto@glv.co.jp>
2024-02-07 15:28:24 +0900

    ヘッダー表示(CSSデザイン除く）

--- front_end_task_first/dist/index.js
+++ front_end_task_first/dist/index.js
@@ -1,13 +1,4 @@
-"use strict";
-class Header {
-    constructor() {
-        this.element = document.createElement('header');
-        this.element.textContent = 'My Website Header';
-    }
-    render(target) {
-        target.appendChild(this.element);
-    }
-}
+import { Header } from './header';
 const header = new Header();
-header.render(document.getElementById('app'));
+header.render(document.getElementById('header'));
 //# sourceMappingURL=index.js.map
--- front_end_task_first/src/index.ts
+++ front_end_task_first/src/index.ts
@@ -1,16 +1,5 @@
-// src/index.ts
-class Header {
-  private element: HTMLElement;
-
-  constructor() {
-      this.element = document.createElement('header');
-      this.element.textContent = 'My Website Header';
-  }
-
-  render(target: HTMLElement) {
-      target.appendChild(this.element);
-  }
-}
+import { Header } from './header';
+const body = document.body;
 
 const header = new Header();
-header.render(document.getElementById('app')!);
+header.render(body);
