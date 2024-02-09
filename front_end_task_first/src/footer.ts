5a377f18b94edd8a9736b9f4b7db34337cc2e361
杉本博陽 <hiroaki.sugimoto@glv.co.jp>
2024-02-09 15:50:53 +0900

    footer部分作成

--- front_end_task_first/src/footer.ts
+++ front_end_task_first/src/footer.ts
@@ -6,6 +6,18 @@ export class Footer {
   constructor() {
     this.element = document.createElement('footer');
     this.element.classList.add('footer-content'); 
+
+    const footer_inner_div = document.createElement('div');
+    footer_inner_div.classList.add('footer_contena');
+    this.element.appendChild(footer_inner_div);
+
+    const footer_copyright = document.createElement('small')
+    footer_copyright.classList.add('footer_copyright');
+    footer_inner_div.appendChild(footer_copyright);
+
+    const footer_item = document.createElement('span')
+    footer_item.textContent = "©2000 - 2024 IDOM Inc."
+    footer_copyright.appendChild(footer_item);
   }
   
   render(target: HTMLElement) {
